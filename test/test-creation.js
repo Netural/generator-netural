300a6fa6ddb10d91c4ef62abdc8334d6e975a32c
Stefan Baumgartner <sbaumg@gmail.com>
2014-12-30 11:30:41 +0100

    tests for #16

--- test/test-creation.js
+++ test/test-creation.js
@@ -40,7 +40,57 @@ describe('netural generator', function () {
         });
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
-            helpers.assertFiles(expected);
+            helpers.assertFile(expected);
+            done();
+        });
+    });
+});
+
+
+
+describe('netural generator - not for netural', function () {
+    beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+            if (err) {
+                return done(err);
+            }
+
+            this.app = helpers.createGenerator('netural:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
+    });
+
+    it('creates expected files', function (done) {
+        var expected = [
+            'package.json',
+            'bower.json',
+            '.editorconfig',
+            '.jshintrc',
+            '.bowerrc',
+            '.gitignore',
+            'Gruntfile.js',
+            'app/scripts/main.js',
+            'app/styles/main.scss',
+            'app/templates/layouts/layout.hbs',
+            'app/templates/pages/index.hbs',
+            'app/templates/partials/scripts.hbs'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'projectName': 'test-project',
+            'includeAutoprefixer': false,
+            'includeNeturalNotice': fasee
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFile(expected);
+            helpers.assertNoFileContent([
+                ['app/scripts/main.js',/\w\w/],
+                ['package.json',/autoprefixer/],
+                ['Gruntfile.js',/autoprefixer/]
+            ]);
             done();
         });
     });
