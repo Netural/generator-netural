66c181a5a01ce033d296eb35f50afc649ecfc736
thomas.pink <thomas.pink@netural.com>
2015-08-19 16:47:02 +0200

    added autoprefixer

--- app/templates/gulp/styles.js
+++ app/templates/gulp/styles.js
@@ -1,8 +1,9 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
-var util        = require('gulp-util');
+var util         = require('gulp-util');
 var globbing     = require('gulp-css-globbing');
+var autoprefixer = require('gulp-autoprefixer');
 var config       = require('./config');
 
 gulp.task('styles', function () {
@@ -11,8 +12,20 @@ gulp.task('styles', function () {
         extensions: ['.scss']
     }))
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .on('error', util.log.bind(util, 'Saas Error'))
+    .pipe(sass().on('error', util.log.bind(util, 'Saas Error')))
+    .pipe(autoprefixer({
+        browsers:  [
+            'ie >= 8',
+            'ie_mob >= 10',
+            'ff >= 30',
+            'chrome >= 34',
+            'safari >= 7',
+            'opera >= 23',
+            'ios >= 7',
+            'android >= 4.4',
+            'bb >= 10'
+        ]
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest.css));
 });
