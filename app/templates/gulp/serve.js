5215a045405443829ce1809baa22e45b7906a0e8
thomas.pink <thomas.pink@netural.com>
2015-08-31 18:10:22 +0200

    vendor + server

--- app/templates/gulp/serve.js
+++ app/templates/gulp/serve.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
 var config = require('./config');
+var server = require('gulp-server-livereload');
 
-gulp.task('serve', ['build', 'watch']);
+gulp.task('serve', ['build', 'watch'], function() {
+    gulp.src('dist')
+        .pipe(server({
+            livereload: true,
+            directoryListing: false,
+            open: true
+        }));
+});
--- app/templates/gulp/vendor.js
+++ app/templates/gulp/vendor.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var gulp         = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var config       = require('./config');
 
 gulp.task('modernizr', function () {
@@ -8,4 +11,11 @@ gulp.task('modernizr', function () {
     .pipe(gulp.dest(config.dest.js+'/vendor'))
 });
 
-gulp.task('vendor', ['modernizr']);
+
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles(), { base: config.src.bower_components })
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(config.dest.js+'/vendor'));
+});
+
+gulp.task('vendor', ['modernizr', 'bower']);
