427c4ff94a791e367f07439d85637976e49e629a
thomas.pink <thomas.pink@netural.com>
2015-09-17 13:26:00 +0200

    fixed controller generator template

--- app/templates/gulp/generator_templates/controller.js
+++ app/templates/gulp/generator_templates/controller.js
@@ -1,6 +1,6 @@
 import Mojito from '../../../bower_components/mojito/lib/main';
 
-export default class <%= name %>Controller extends Mojito.Controller {
+export default class <%= name %> extends Mojito.Controller {
 
     constructor(...args) {
         super(args);
